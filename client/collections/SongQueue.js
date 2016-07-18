399c9dd7737a83c349b289fe3296a985d3cddfae
Sam Landfried <samlandfried@gmail.com>
2016-07-18 18:27:28 +0700

    Enqueue songs when clicked (Disable play on click)

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -3,7 +3,9 @@ var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
 
+  // When does this fire? When a song is added, or when its first created? Im betting created.
   initialize: function() {
+
   }
 
 });
