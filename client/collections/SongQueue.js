f0bd41791e923bd5a102536637b051a210797ee9
Sam Landfried <samlandfried@gmail.com>
2016-07-18 19:04:09 +0700

    Play first song in queue and pass spec

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -5,7 +5,15 @@ var SongQueue = Backbone.Collection.extend({
 
   // When does this fire? When a song is added, or when its first created? Im betting created.
   initialize: function() {
+    this.on('add', function () {
+      if ( this.length === 1 ) {
+        this.playFirst(this.models[0]);
+      }
+    })
+  },
 
+  playFirst: function(song) {
+    song.play();
   }
 
 });
