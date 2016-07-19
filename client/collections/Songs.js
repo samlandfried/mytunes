568b8911521a162e3bbcf5c401b48946980662e2
Sam Landfried <samlandfried@gmail.com>
2016-07-19 18:14:20 +0700

    Get songs from Parse but...
    Can't figure out how to retain access to the list from within a function

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,16 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
+
+  initialize: function () {
+    $.get('https://api.parse.com/1/classes/songs/', data => this.buildLibrary(data.results));
+  },
+
+  buildLibrary: data => {
+    var self = this;
+    console.log(self);
+  }
+    
 
 });
