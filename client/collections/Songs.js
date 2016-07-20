992fa8e17c49f34ff02a05c5241dafb97eb40daa
Sam Landfried <samlandfried@gmail.com>
2016-07-20 15:07:30 +0700

    Give up on querying Parse server...
    Either its impossible to place a POST request to this server, or I'm formatting the request incorrectly. Either way, I'm going to do a sloppier solution of retrieving all results and filtering them.

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -3,7 +3,7 @@ var Songs = Backbone.Collection.extend({
 
   model: SongModel,
 
-  initialize: function () {
+  initialize: function() {
     $.get('https://api.parse.com/1/classes/songs/', data => {
       data.results.map(song => {
         song.playCount = 0;
@@ -13,7 +13,15 @@ var Songs = Backbone.Collection.extend({
     });
   },
 
-  search: function (query) {
-    console.log(query);
+  search: function(query) {
+    // This is responding with a 403 forbidden error. I'm going to assume that its impossible to make a post request to this server, although its possible (likely?) I'm just formatting it incorrectly. 
+    // $.ajax({
+    //   type: "POST",
+    //   url: 'https://api.parse.com/1/classes/songs/',
+    //   data: JSON.stringify({artist: 'Aaliyah'}),
+    //   success: data => console.log(data),
+    //   failure: data => console.log(data),
+    //   dataType: 'application/json'
+    // });
   }
 });
--- client/views/SearchView.js
+++ client/views/SearchView.js
@@ -1,7 +1,7 @@
 var SearchView = Backbone.View.extend({
 
   el: `<form id="search">
-        <input type="text" name="input">
+        <input type="text" value="Back and Forth">
         <input type="submit" class="searchbutton" value="Search">
         <br>
       </form>`,
@@ -11,7 +11,7 @@ var SearchView = Backbone.View.extend({
     this.$el.submit(event => {
       event.preventDefault();
       this.collection.search(event.target[0].value);
-    })
+    });
 
     // .on('click', () => {
     //   this.collection.search(this.$el.children()[0].value);
