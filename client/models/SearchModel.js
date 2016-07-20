304e7d4ac8be538337414c497a5e761b08c96c79
Sam Landfried <samlandfried@gmail.com>
2016-07-20 16:50:45 +0700

    Delete SearchModel (Unnecessary)

--- client/models/SearchModel.js
+++
@@ -1,6 +0,0 @@
-var SearchModel = Backbone.Model.extend({
-  // This might be unnecessary. SearchView clicks should activate a search from Songs
-  search: function() {
-    this.trigger();
-  }
-});
